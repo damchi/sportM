c704b1eebb6b21a74e1acb39d257e878af468e08
damien <damien.raffi@me.com>
2020-05-06 21:24:04 -0400

    latest

--- sportM/amplify/backend/function/functionCreateFirstTraining/src/index.js
+++ sportM/amplify/backend/function/functionCreateFirstTraining/src/index.js
@@ -1,10 +1,22 @@
-import {Storage} from "aws-amplify";
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var functionFunctionCreateFirstTrainingName = process.env.FUNCTION_FUNCTIONCREATEFIRSTTRAINING_NAME
+var storageTrainingDescriptionBucketName = process.env.STORAGE_TRAININGDESCRIPTION_BUCKETNAME
+var storageTrainingDescriptionBucketName = process.env.STORAGE_TRAININGDESCRIPTION_BUCKETNAME
+var apiSportmTrainingTableName = process.env.API_SPORTM_TRAININGTABLE_NAME
+var apiSportmTrainingTableArn = process.env.API_SPORTM_TRAININGTABLE_ARN
+var apiSportmGraphQLAPIIdOutput = process.env.API_SPORTM_GRAPHQLAPIIDOUTPUT
+
+Amplify Params - DO NOT EDIT *///import {Storage} from "aws-amplify";
+
 
 exports.handler = function(event, context) {
+  let client = AWS.S3()
   //eslint-disable-line
-  // this.CreateTraining()
   console.log(JSON.stringify(event, null, 2));
-  Storage.list('new/').then(result => console.log(result));
+  //Storage.get('new/tt.json').then(result => console.log(result));
   event.Records.forEach(record => {
     console.log('logs lambda');
     console.log('records:' + JSON.stringify(record));
